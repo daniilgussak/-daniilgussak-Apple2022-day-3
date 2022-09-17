663aa2bce621f8dc44a705e45cce5a04f1e7a6b8
daniilgussak <104781681+daniilgussak@users.noreply.github.com>
2022-09-18 01:19:28 +0600

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+import Swiper from '../lib/swiper-bundle.esm.browser.min.js';
+
+new Swiper('.goods__block', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+        320:{
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 2,
+            spaceBetween: 24,
+        },
+        1140: {
+            slidesPerView: 3,
+            spaceBetween: 24,
+        },
+    },
+    
+    navigation: {
+        prevEl: '.goods__arrow-pre',
+        nextEl: '.goods__arrow-next',
+    }
+});
